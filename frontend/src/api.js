9a95040be4c66021cb7a61715cd74f11e3a678fa
Jyasi Davis <genusglobalinc@gmail.com>
2025-04-11 13:40:06 -0400

    register flow cleanup

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://lost-gates-mvp.onrender.com:8000';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
