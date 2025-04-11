3d188069ea5d59c9d92dbc76e8089f844d60f11b
Jyasi Davis <genusglobalinc@gmail.com>
2025-04-11 04:48:20 -0400

    front end react fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Feed from './components/Feed';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route path="/" exact component={Feed} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/profile" component={Profile} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </div>
     </Router>
   );
