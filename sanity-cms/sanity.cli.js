72c376a6b7c4f56f5784a97a115a9275e88c8530
Jyasi Davis <genusglobalinc@gmail.com>
2025-04-30 14:12:03 -0400

    adding sanity to backend for posts and users

--- sanity-cms/sanity.cli.js
+++ sanity-cms/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '0na7kv87',
+    projectId: 'jpgxw2o8',
     dataset: 'production'
   },
   /**
